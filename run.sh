bfac7003ecab813923ac49d804d00d2671a3abea
mnod <mnod@example.com>
2017-03-08 18:57:00 +0900

    modify run.sh

--- run.sh
+++ run.sh
@@ -4,14 +4,6 @@ PASS=`pwgen -s 12 1`
 echo ${PASS}
 echo docker:${PASS} | chpasswd
 
-vncdir=~docker/.vnc
-vncpassfile=${vncdir}/passwd
-mkdir ${vncdir}
-chmod 700 ${vncdir}
-echo ${PASS} | vncpasswd -f > ${vncpassfile}
-chmod 600 ${vncpassfile}
-chown -R docker:docker ${vncdir}
-
 if [ $# -eq 1 ]; then
   sshdir=~docker/.ssh
   keyfile=$sshdir/authorized_keys
@@ -21,5 +13,13 @@ if [ $# -eq 1 ]; then
   chown -R docker:docker $sshdir
 fi
 
-su - docker -c "LANG=ja_JP.UTF-8 vncserver -localhost -geometry 1366x720 -depth 8 :1"
+vncdir=~docker/.vnc
+vncpassfile=${vncdir}/passwd
+mkdir ${vncdir}
+chmod 700 ${vncdir}
+echo ${PASS} | vncpasswd -f > ${vncpassfile}
+chmod 600 ${vncpassfile}
+chown -R docker:docker ${vncdir}
+
+su - docker -c "vncserver -localhost -geometry 1366x720 -depth 8 :1"
 /usr/sbin/sshd -D
